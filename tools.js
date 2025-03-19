c0ca2253d2c634135a21a9b06a9408e6cea2c2ca
TraceyChen927 <traceychen927@gmail.com>
2025-03-18 21:58:43 -0400

    Add files via upload
    Signed-off-by: TraceyChen927 <traceychen927@gmail.com>

---
+++ tools.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const firstimage = document.getElementById("onebrush"); /* moving up and fade in */
+
+    firstimage.classList.add("show");
+
+    void firstimage.offsetWidth;
+
+    firstumage.classList.add("show");
+});
