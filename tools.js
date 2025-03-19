446f0321566097c8975a223811bd1075cdeca8a0
TraceyChen927 <traceychen927@gmail.com>
2025-03-18 21:58:34 -0400

    Delete tools.js
    Signed-off-by: TraceyChen927 <traceychen927@gmail.com>

--- tools.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const firstimage = document.getElementById("onebrush");
-
-    firstimage.classList.add("show");
-
-    void firstimage.offsetWidth;
-
-    firstumage.classList.add("show");
-});
